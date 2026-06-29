1c43c04b393f59c758551704e01275d459e475f2
tetradox1412 <mriduljoy1412@gmail.com>
2026-06-29 22:30:13 +0530

    Time: 2 ms (77.02%), Space: 48.6 MB (41.15%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -3,18 +3,13 @@ public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> ans;
         int n = nums.size();
-        for (int j = 0; j < 4; j++)
-            for (int i = 0; i < n; i++)
-                swap(nums[i], nums[nums[i] - 1]);
-
-        for (auto x : nums)
-            cout << x << " ";
-        cout << endl;
-
-        for (int i = 0; i < n; i++)
-            if (nums[i] != i + 1)
-                ans.push_back(nums[i]);
-
+        for (int i = 0; i < n; i++) {
+            int x = abs(nums[i]) - 1;
+            if (nums[x] > 0)
+                nums[x] = -1 * nums[x];
+            else
+                ans.push_back(abs(nums[i]));
+        }
         return ans;
     }
 };
