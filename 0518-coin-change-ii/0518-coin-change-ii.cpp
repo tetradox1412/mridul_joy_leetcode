5b79a14107ccc1c4c78663fb2a6ecb31acebc16e
tetradox1412 <mriduljoy1412@gmail.com>
2026-07-11 12:49:45 +0530

    Time: 8 ms (80.66%), Space: 10.1 MB (96.59%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<unsigned int> dp(amount + 1, 0);
+        dp[0] = 1;
+        for (int i = 0; i < coins.size(); i++)
+            for (int j = 1; j <= amount; j++)
+                if (coins[i] <= j)
+                    dp[j] += dp[j - coins[i]];
+        return dp[amount];
+    }
+};
