9d61c76a70a4e446528509a505ec8625ae29ffc2
tetradox1412 <mriduljoy1412@gmail.com>
2026-07-15 15:28:33 +0530

    Time: 16 ms (80.46%), Space: 102.9 MB (63.51%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<pair<int, int>> st;
+        stack<int> st;
         int n = temperatures.size();
 
         vector<int> answer(n, 0);
-        st.push(make_pair(temperatures[n - 1], n - 1));
+        st.push(n - 1);
         for (int i = n - 2; i >= 0; i--) {
-            while (!st.empty() && st.top().first <= temperatures[i])
+            while (!st.empty() && temperatures[st.top()] <= temperatures[i])
                 st.pop();
 
             if (!st.empty())
-                answer[i] = st.top().second - i;
-            st.push(make_pair(temperatures[i], i));
+                answer[i] = st.top() - i;
+            st.push(i);
         }
 
         return answer;
