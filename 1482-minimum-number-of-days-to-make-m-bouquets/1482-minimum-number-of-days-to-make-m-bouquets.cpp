0e343d3c717d83ae3cc2d4d2508226c18d34bdd4
tetradox1412 <mriduljoy1412@gmail.com>
2026-07-15 12:42:18 +0530

    Time: 20 ms (70.21%), Space: 70.5 MB (79.1%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -19,17 +19,12 @@ public:
             if (minDays == maxDays)
                 return maxDays;
 
-            vector<int> hasFlowerBloomed(n, 0);
             int currDays = minDays + (maxDays - minDays) / 2;
-
-            for (int i = 0; i < n; i++)
-                if (bloomDay[i] <= currDays)
-                    hasFlowerBloomed[i] = 1;
 
             int counter = 0;
             int bouquetsFormed = 0;
             for (int i = 0; i < n; i++) {
-                if (hasFlowerBloomed[i] == 1)
+                if (bloomDay[i] <= currDays)
                     counter++;
                 else {
                     bouquetsFormed += counter / k;
