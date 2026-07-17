8644a2bd7b78304094e40a0dd903c915dbaa8724
tetradox1412 <mriduljoy1412@gmail.com>
2026-07-17 18:32:31 +0530

    Time: 20 ms (90.31%), Space: 39.7 MB (80.48%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -8,8 +8,7 @@ public:
         for (int i = 0; i < n; i++)
             tracker.push_back({arr[i], i});
 
-        sort(tracker.begin(), tracker.end(),
-             [](const auto& a, const auto& b) { return a.first < b.first; });
+        sort(tracker.begin(), tracker.end());
 
         int rank = 1;
         arr[tracker[0].second] = rank;
