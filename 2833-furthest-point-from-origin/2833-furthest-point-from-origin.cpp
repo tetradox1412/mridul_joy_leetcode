0bf4f2dd7734b7378ec88cce9390fa5bb35cb398
tetradox1412 <mriduljoy1412@gmail.com>
2026-07-24 13:10:50 +0530

    Time: 0 ms (100%), Space: 9.1 MB (66.19%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int furthestDistanceFromOrigin(string moves) {
-        // 'L' -> idx 0. 'R' -> idx 1. '_' -> idx 2
-        vector<int> countMoves(3, 0);
+        int left = 0;
+        int right = 0;
+        int blanks = 0;
 
         for (auto x : moves) {
             if (x == 'L')
-                countMoves[0]++;
+                left++;
             else if (x == 'R')
-                countMoves[1]++;
+                right++;
             else
-                countMoves[2]++;
+                blanks++;
         }
 
-        return abs(countMoves[0] - countMoves[1]) + countMoves[2];
+        return abs(left - right) + blanks;
     }
 };
