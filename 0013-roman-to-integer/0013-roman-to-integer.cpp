ad1c42430524b2c7df140921ed55daf423011959
tetradox1412 <mriduljoy1412@gmail.com>
2026-07-24 18:38:20 +0530

    Time: 0 ms (100%), Space: 12.2 MB (71.03%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -14,28 +14,12 @@ public:
         int sum = 0;
         int idx = 0;
         while (idx < n) {
-            if (s[idx] == 'I' && idx != n - 1 && s[idx + 1] == 'V') {
-                sum += 4;
-                idx += 2;
-            } else if (s[idx] == 'I' && idx != n - 1 && s[idx + 1] == 'X') {
-                sum += 9;
-                idx += 2;
-            } else if (s[idx] == 'X' && idx != n - 1 && s[idx + 1] == 'L') {
-                sum += 40;
-                idx += 2;
-            } else if (s[idx] == 'X' && idx != n - 1 && s[idx + 1] == 'C') {
-                sum += 90;
-                idx += 2;
-            } else if (s[idx] == 'C' && idx != n - 1 && s[idx + 1] == 'D') {
-                sum += 400;
-                idx += 2;
-            } else if (s[idx] == 'C' && idx != n - 1 && s[idx + 1] == 'M') {
-                sum += 900;
-                idx += 2;
-            } else {
+            if (idx != n - 1 && map[s[idx]] < map[s[idx + 1]])
+                sum -= map[s[idx]];
+            else
                 sum += map[s[idx]];
-                idx++;
-            }
+
+            idx++;
         }
         return sum;
     }
