018e079e777a551a83dc182f7e400e8445d51364
tetradox1412 <mriduljoy1412@gmail.com>
2026-07-24 20:20:14 +0530

    Time: 17 ms (53.39%), Space: 27.2 MB (22.4%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans;
-        unordered_map<string, vector<int>> map;
+        unordered_map<string, vector<string>> map;
         int n = strs.size();
 
-        vector<string> strCpy;
         for (auto x : strs) {
+            string p = x;
             sort(x.begin(), x.end());
-            strCpy.push_back(x);
+            map[x].push_back(p);
         }
-
-        for (int i = 0; i < n; i++)
-            map[strCpy[i]].push_back(i);
 
         for (auto x : map) {
             vector<string> temp;
             for (auto y : x.second)
-                temp.push_back(strs[y]);
+                temp.push_back(y);
 
             ans.push_back(temp);
         }
