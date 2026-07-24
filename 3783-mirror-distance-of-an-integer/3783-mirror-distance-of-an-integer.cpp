9bbfc856c2b12d3e3786ef6810cfe668f0561fb9
tetradox1412 <mriduljoy1412@gmail.com>
2026-07-24 13:15:58 +0530

    Time: 0 ms (100%), Space: 8.6 MB (26.92%) - LeetHub

---
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int n) {
+        int rev = 0;
+        while (n > 0) {
+            rev = rev * 10 + (n % 10);
+            n = n / 10;
+        }
+        return rev;
+    }
+
+    int mirrorDistance(int n) { return abs(n - reverse(n)); }
+};
