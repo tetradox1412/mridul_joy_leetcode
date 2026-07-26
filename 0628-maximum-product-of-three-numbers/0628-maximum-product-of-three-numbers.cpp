94d45468e976cf282a778b613e678d00a1dd4004
tetradox1412 <mriduljoy1412@gmail.com>
2026-07-26 12:43:49 +0530

    Time: 0 ms (100%), Space: 31.4 MB (96.8%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -6,10 +6,38 @@ public:
         if (n == 3)
             return nums[0] * nums[1] * nums[2];
 
-        sort(nums.begin(), nums.end());
-        return max(max(nums[0] * nums[1] * nums[n - 1],
-                       nums[n - 1] * nums[n - 2] * nums[0]),
-                   max(nums[n - 1] * nums[n - 2] * nums[n - 3],
-                       nums[0] * nums[1] * nums[2]));
+        int large_1 = -1001;
+        int large_2 = -1001;
+        int large_3 = -1001;
+
+        int small_1 = 1001;
+        int small_2 = 1001;
+        int small_3 = 1001;
+
+        for (auto x : nums) {
+            if (x > large_1) {
+                large_3 = large_2;
+                large_2 = large_1;
+                large_1 = x;
+            } else if (x > large_2) {
+                large_3 = large_2;
+                large_2 = x;
+            } else if (x > large_3)
+                large_3 = x;
+
+            if (x < small_1) {
+                small_3 = small_2;
+                small_2 = small_1;
+                small_1 = x;
+            } else if (x < small_2) {
+                small_3 = small_2;
+                small_2 = x;
+            } else if (x < small_3)
+                small_3 = x;
+        }
+
+        return max(
+            max(large_1 * large_2 * large_3, small_1 * small_2 * small_3),
+            max(large_1 * large_2 * small_1, small_1 * small_2 * large_1));
     }
 };
