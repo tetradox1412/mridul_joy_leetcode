ba2622032c8ef9e206eec2c38f7c3e2abd00fe16
tetradox1412 <mriduljoy1412@gmail.com>
2026-07-26 12:51:13 +0530

    Time: 0 ms (100%), Space: 31.6 MB (10.45%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -6,38 +6,34 @@ public:
         if (n == 3)
             return nums[0] * nums[1] * nums[2];
 
-        int large_1 = -1001;
-        int large_2 = -1001;
-        int large_3 = -1001;
-
-        int small_1 = 1001;
-        int small_2 = 1001;
-        int small_3 = 1001;
+        vector<int> large(3, -1001);
+        vector<int> small(3, 1001);
 
         for (auto x : nums) {
-            if (x > large_1) {
-                large_3 = large_2;
-                large_2 = large_1;
-                large_1 = x;
-            } else if (x > large_2) {
-                large_3 = large_2;
-                large_2 = x;
-            } else if (x > large_3)
-                large_3 = x;
+            if (x > large[0]) {
+                large[2] = large[1];
+                large[1] = large[0];
+                large[0] = x;
+            } else if (x > large[1]) {
+                large[2] = large[1];
+                large[1] = x;
+            } else if (x > large[2])
+                large[2] = x;
 
-            if (x < small_1) {
-                small_3 = small_2;
-                small_2 = small_1;
-                small_1 = x;
-            } else if (x < small_2) {
-                small_3 = small_2;
-                small_2 = x;
-            } else if (x < small_3)
-                small_3 = x;
+            if (x < small[0]) {
+                small[2] = small[1];
+                small[1] = small[0];
+                small[0] = x;
+            } else if (x < small[1]) {
+                small[2] = small[1];
+                small[1] = x;
+            } else if (x < small[2])
+                small[2] = x;
         }
 
         return max(
-            max(large_1 * large_2 * large_3, small_1 * small_2 * small_3),
-            max(large_1 * large_2 * small_1, small_1 * small_2 * large_1));
+            max(large[0] * large[1] * large[2], small[0] * small[1] * small[2]),
+            max(large[0] * large[1] * small[0],
+                small[0] * small[1] * large[0]));
     }
 };
