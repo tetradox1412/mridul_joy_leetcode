6feb6bd49653d26e34bbfaf2a875376b5778a279
tetradox1412 <mriduljoy1412@gmail.com>
2026-07-28 17:01:41 +0530

    Time: 16 ms (94.73%), Space: 54.2 MB (91.65%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -9,7 +9,7 @@ public:
         for (auto& x : s)
             vec[x - 'a']++;
 
-        char odd_char = 'a';
+        char odd_char;
         int odd_char_present = 0;
         for (int i = 0; i < 26; i++)
             if (vec[i] % 2) {
@@ -18,23 +18,19 @@ public:
                 break;
             }
 
-        vector<char> ans(n);
         int idx = 0;
         for (int i = 0; i < 26; i++) {
             while (vec[i] > 1) {
                 vec[i] -= 2;
-                ans[idx] = 'a' + i;
-                ans[n - 1 - idx] = 'a' + i;
+                s[idx] = 'a' + i;
+                s[n - 1 - idx] = 'a' + i;
                 idx++;
             }
         }
 
-        for (int i = idx; i <= n - 1 - idx && odd_char_present; i++)
-            ans[i] = odd_char;
+        if (odd_char_present)
+            s[idx] = odd_char;
 
-        string answer;
-        for (auto& x : ans)
-            answer += x;
-        return answer;
+        return s;
     }
 };
