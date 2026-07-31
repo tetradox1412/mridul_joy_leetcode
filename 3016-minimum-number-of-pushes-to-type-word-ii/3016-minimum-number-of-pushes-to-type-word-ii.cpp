152fa342979c8b997dd888b592fea7448b60840e
tetradox1412 <mriduljoy1412@gmail.com>
2026-07-31 14:27:04 +0530

    Time: 15 ms (35.66%), Space: 28.5 MB (15.83%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -4,19 +4,13 @@ public:
         vector<int> vec(26, 0);
         for (auto& x : word)
             vec[x - 'a']++;
+        sort(vec.rbegin(), vec.rend());
+
         int sum = 0;
         int tracker = 0;
-        int count = word.size();
-        
-        while (count > 0) {
-            int max_idx = 0;
-            for (int i = 1; i < 26; i++)
-                max_idx = vec[i] > vec[max_idx] ? i : max_idx;
-            count -= vec[max_idx];
-            int char_count = vec[max_idx];
-            vec[max_idx] = 0;
+
+        for (auto& char_count : vec)
             sum += ((tracker++ / 8) + 1) * char_count;
-        }
 
         return sum;
     }
