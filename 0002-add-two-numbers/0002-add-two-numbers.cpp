9efee08767305c31fe6c262d60828159a5c2d43d
tetradox1412 <mriduljoy1412@gmail.com>
2026-08-25 18:35:17 +0530

    Time: 2 ms (38.93%), Space: 77 MB (93.91%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* createNode(int n) {
+        ListNode* l = new ListNode();
+        l->val = n;
+        l->next = NULL;
+        return l;
+    }
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        string s;
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2;
+        int carry = 0;
+        int val;
+        while (temp1 != NULL && temp2 != NULL) {
+            val = temp1->val + temp2->val + carry;
+            s += (val % 10) + '0';
+            carry = val / 10;
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+
+        while (temp1 != NULL) {
+            val = temp1->val + carry;
+            s += (val % 10) + '0';
+            carry = val / 10;
+            temp1 = temp1->next;
+        }
+
+        while (temp2 != NULL) {
+            val = temp2->val + carry;
+            s += (val % 10) + '0';
+            carry = val / 10;
+            temp2 = temp2->next;
+        }
+
+        if (carry)
+            s += carry + '0';
+
+        ListNode* ans = createNode(s[0] - '0');
+        ListNode* tail = ans;
+        for (int i = 1; i < s.size(); i++) {
+            ListNode* a = createNode(s[i] - '0');
+            tail->next = a;
+            tail = a;
+        }
+        return ans;
+    }
+};
