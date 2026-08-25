7d85836d7c007e233e521012c131eb7d8dd7539d
tetradox1412 <mriduljoy1412@gmail.com>
2026-08-25 21:13:06 +0530

    Time: 350 ms (4.68%), Space: 27.2 MB (26.2%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void printArr(vector<int>& vec) {
+        for (auto& x : vec)
+            cout << x << " ";
+        cout << "\n";
+    }
+
+    int trap(vector<int>& height) {
+        int n = height.size();
+
+        if (n <= 2)
+            return 0;
+
+        vector<int> leftMax(n);
+        vector<int> rightMax(n);
+
+        leftMax[0] = -1;
+        rightMax[n - 1] = -1;
+
+        int left = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = left;
+            left = max(left, height[i]);
+        }
+
+        int right = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = right;
+            right = max(right, height[i]);
+        }
+
+        printArr(leftMax);
+        printArr(rightMax);
+
+        int net = 0;
+        for (int i = 1; i < n - 1; i++)
+            net += max(0, min(leftMax[i], rightMax[i]) - height[i]);
+
+        return net;
+    }
+};
