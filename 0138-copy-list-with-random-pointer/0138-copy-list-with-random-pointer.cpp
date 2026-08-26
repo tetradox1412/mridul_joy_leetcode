a04e7262e29b76a27ee8978dedf9e6d7d7495b59
tetradox1412 <mriduljoy1412@gmail.com>
2026-08-26 16:48:13 +0530

    Time: 3 ms (91.27%), Space: 15.3 MB (28.86%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,37 +17,30 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        vector<Node*> list_org;
-        vector<Node*> ans_list;
-        unordered_map<Node*, Node*> map;
-
         if (head == NULL)
-            return NULL;
+            return head;
 
-        Node* temp = head;
-        while (temp != NULL) {
-            list_org.push_back(temp);
+        unordered_map<Node*, Node*> map;
+        Node* ans = new Node(head->val);
+        map[head] = ans;
+        Node* temp = ans;
+        Node* tempHead = head;
+        while (tempHead->next != NULL) {
+            Node* a = new Node(tempHead->next->val);
+            temp->next = a;
+            temp = a;
+            map[tempHead->next] = a;
+            tempHead = tempHead->next;
+        }
+        temp = ans;
+        while (head != NULL) {
+            if (head->random == NULL)
+                temp->random == NULL;
+            else
+                temp->random = map[head->random];
+            head = head->next;
             temp = temp->next;
         }
-
-        int n = list_org.size();
-        for (int i = 0; i < n; i++) {
-            Node* a = new Node(list_org[i]->val);
-            map[list_org[i]] = a;
-            ans_list.push_back(a);
-        }
-
-        for (int i = 0; i < n - 1; i++)
-            ans_list[i]->next = ans_list[i + 1];
-        ans_list[n - 1]->next = NULL;
-
-        for (int i = 0; i < n; i++) {
-            if (list_org[i]->random == NULL)
-                ans_list[i]->random = NULL;
-            else
-                ans_list[i]->random = map[list_org[i]->random];
-        }
-
-        return ans_list[0];
+        return ans;
     }
 };
